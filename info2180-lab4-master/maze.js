96538be0b17733ad9124a6259f7d8f4e5843671c
Shanese Johnson <Shanese Johnson>
2016-10-14 20:28:34 -0500

    Exercise 2

--- info2180-lab4-master/maze.js
+++ info2180-lab4-master/maze.js
@@ -1,10 +1,17 @@
 window.onload = function()
 	{
-		var boundary=document.getElementById('boundary1');
-		boundary.onmouseover = function()
+		var boundary=document.querySelectorAll('.boundary');
+		for (var i = 0; i <boundary.length; i++) 
+			{
+				boundary[i].onmouseover = function()
 
-		{
-			boundary.setAttribute("class","boundary youlose");
-		}
-
+				{
+					for (var x = 0; x < boundary.length; x++) {
+						boundary[x].setAttribute("class","boundary youlose");
+					}
+				}
+ 			} 
 	}
+
+
+
