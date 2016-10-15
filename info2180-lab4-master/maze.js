24e8e7fc4fa307cb8b4b9df5c269df8dc21d37f7
Shanese Johnson <Shanese Johnson>
2016-10-14 20:04:50 -0500

    First commit

---
+++ info2180-lab4-master/maze.js
@@ -0,0 +1,10 @@
+window.onload = function()
+	{
+		var boundary=document.getElementById('boundary1');
+		boundary.onmouseover = function()
+
+		{
+			boundary.setAttribute("class","boundary youlose");
+		}
+
+	}
